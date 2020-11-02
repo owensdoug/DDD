717ff610f02134a3ec223ee1937f3769a4b66ed4
Doug Owens <dougowens@gmail.com>
2020-11-02 09:50:01 -0500

    Add project files.

---
+++ Core.User.Test/Bulider/UserBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace OwensDoug.Core.User.Test.Bulider
+{
+    public class UserBuilder
+    {
+        public int UserId1 => 123;
+        public int UserId2 => 124;
+        public int UserId3 => 125;
+        public string Name1 => "NameX";
+        public string Name2 => "NameY";
+        public string Name3 => "NameZ";
+
+        public List<Entity.UserEntity> GetUserCollection()
+        {
+            return new List<Entity.UserEntity>()
+            {
+                Entity.UserEntity.Create(UserId1, Name1),
+                Entity.UserEntity.Create(UserId2, Name2),
+                Entity.UserEntity.Create(UserId3, Name3)
+            };
+        }
+    }
+}
